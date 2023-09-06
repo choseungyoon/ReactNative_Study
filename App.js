90907c89f5e7f8ea5a89e56a4047fcf1eadfc9c0
choseungyoon <cho31250@gmail.com>
2023-09-06 17:57:10 +0900

    Cusotm Hook 실습

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { StyleSheet, View,Text, TextInput, Button} from 'react-native';
 import React, { useState } from 'react';
+import CustomHook from './CustomHook';
 
 export default function App() {
   const [name, setName] = useState('');
@@ -7,22 +8,7 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <TextInput
-        placeholder="이름을 입력하세요"
-        onChangeText={input => setName(input)}
-        value={name}
-      />
-      <TextInput
-        placeholder="나이를 입력하세요"
-        onChangeText={input => setAge(input)}
-        value={age}
-      />
-      <Button
-        title="제출"
-        onPress={() => alert(`이름: ${name}, 나이: ${age}`)}
-      />
-      <Text>이름: {name}</Text>
-      <Text>나이: {age}</Text>
+      <CustomHook></CustomHook>
     </View>
   );
 };
