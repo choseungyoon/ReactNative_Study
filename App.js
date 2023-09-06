c7010067a23a8ad7b85541d75fa29245317a2197
choseungyoon <cho31250@gmail.com>
2023-09-06 14:30:38 +0900

    컴포넌트/Props 실습 완성 코드

--- App.js
+++ App.js
@@ -1,9 +1,54 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View} from 'react-native';
+import { StyleSheet, View,Text,Image} from 'react-native';
+
+
+const Header = (props) => {
+  return <Text>{props.title}</Text>
+}
+
+const Division = () => {
+  return <Text>Division</Text>
+}
+const FriendSection = () => {
+  return <Text>FriendSection</Text>
+}
+const FriendList = () => {
+  return <View>
+      <Profile name="강아지" uri= "https://pds.joongang.co.kr/news/component/htmlphoto_mmdata/201901/20/28017477-0365-4a43-b546-008b603da621.jpg" profileSize = {70}></Profile>
+      <Profile name="강아지" uri= "https://pds.joongang.co.kr/news/component/htmlphoto_mmdata/201901/20/28017477-0365-4a43-b546-008b603da621.jpg" profileSize = {70}></Profile>
+      <Profile name="강아지" uri= "https://pds.joongang.co.kr/news/component/htmlphoto_mmdata/201901/20/28017477-0365-4a43-b546-008b603da621.jpg" profileSize = {70}></Profile>
+      <Profile name="강아지" uri= "https://pds.joongang.co.kr/news/component/htmlphoto_mmdata/201901/20/28017477-0365-4a43-b546-008b603da621.jpg" profileSize = {70}></Profile>
+      <Profile name="강아지" uri= "https://pds.joongang.co.kr/news/component/htmlphoto_mmdata/201901/20/28017477-0365-4a43-b546-008b603da621.jpg" profileSize = {70}></Profile>
+  </View>
+}
+
+const Profile = (props) => {
+  return (
+    <View style={{flexDirection:"row"}}>
+      <Image source={{uri:props.uri}}
+       style={{
+        width:props.profileSize,
+        height:props.profileSize
+       }}></Image>
+       <Text>{props.name}</Text>
+       <Text>dsf</Text>
+    </View>
+  )
+}
 
 export default function App() {
   return (
-    <View style={styles.container}></View>
+    <View style={styles.container}>
+      <Header title="친구"></Header>
+      <Profile 
+      name="내 이름" 
+      uri= "https://pds.joongang.co.kr/news/component/htmlphoto_mmdata/201901/20/28017477-0365-4a43-b546-008b603da621.jpg"
+      profileSize = {100}
+      ></Profile>
+      <Division></Division>
+      <FriendSection></FriendSection>
+      <FriendList></FriendList>
+    </View>
   );
 }
 
